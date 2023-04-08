8cf1a7b965867f3bfe8e761f9efe8c8866761689
Mohamed Oun <mohamed3on@gmail.com>
2023-04-08 21:29:53 +0200

    add cache

--- api/locations.py
+++ api/locations.py
@@ -10,6 +10,7 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
+        self.send_header('Cache-Control', 's-maxage=86400')
         self.end_headers()
         locations = get_popular_friends_locations()
         self.wfile.write(locations.encode())
