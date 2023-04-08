2901421690ce6ed926dc4202a63ccd71235a2aec
Mohamed Oun <mohamed3on@gmail.com>
2023-04-08 21:22:34 +0200

    client side rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect, useDeferredValue, startTransition } from 'react';
+type Location = [string, number];
+type Locations = Location[];
 
-type Props = {
-  locations: Array<[string, number]>;
-};
+export default function Home() {
+  const [locations, setLocations] = useState<Locations | null>(null);
+  const [pending, setPending] = useState(false);
 
-export default function Home({ locations }: Props) {
-  if (locations) {
+  useEffect(() => {
+    setPending(true);
+    fetch(`${process.env.NEXT_PUBLIC_FUNCTIONS_BASE_URL}/api/locations`)
+      .then((response) => response.json())
+      .then((locations) => {
+        startTransition(() => {
+          setLocations(locations);
+          setPending(false);
+        });
+      });
+  }, []);
+
+  const deferredLocations = useDeferredValue(locations);
+
+  if (pending) {
+    return <div className='text-center mt-20'>Loading...</div>;
+  }
+
+  if (deferredLocations) {
     return (
       <div className='min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12'>
         <div className='relative py-3 sm:max-w-xl sm:mx-auto'>
@@ -15,7 +34,7 @@ export default function Home({ locations }: Props) {
               Popular Twitter Friends Locations
             </h1>
             <ul className='space-y-4'>
-              {locations.map((location, index) => (
+              {deferredLocations.map((location, index) => (
                 <li key={index} className='flex justify-between items-center'>
                   <span className='text-xl font-semibold'>{location[0]}</span>
                   <span className='bg-blue-500 text-white font-semibold py-1 px-3 rounded'>
@@ -30,24 +49,4 @@ export default function Home({ locations }: Props) {
     );
   }
   return null;
-}
-
-export async function getServerSideProps() {
-  try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_FUNCTIONS_BASE_URL}/api/locations`);
-    const locations = await response.json();
-
-    return {
-      props: {
-        locations,
-      },
-    };
-  } catch (error) {
-    console.error(error);
-    return {
-      props: {
-        locations: null,
-      },
-    };
-  }
 }
