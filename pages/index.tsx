3f02f9b593a5ea3c5a38340b5f05383165e2a06c
Mohamed Oun <mohamed3on@gmail.com>
2023-04-08 21:16:09 +0200

    fix type

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-export default function Home({ locations }) {
+type Props = {
+  locations: Array<[string, number]>;
+};
+
+export default function Home({ locations }: Props) {
   if (locations) {
     return (
       <div className='min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12'>
