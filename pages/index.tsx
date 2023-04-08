364a1bf3b85404a00453d4cd4b269913a619f0c7
Mohamed Oun <mohamed3on@gmail.com>
2023-04-08 21:39:56 +0200

    page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import React, { useState, useEffect, startTransition } from 'react';
 type Location = [string, number];
 type Locations = Location[];
@@ -24,26 +25,31 @@ export default function Home() {
 
   if (locations) {
     return (
-      <div className='min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12'>
-        <div className='relative py-3 sm:max-w-xl sm:mx-auto'>
-          <div className='absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl'></div>
-          <div className='relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20'>
-            <h1 className='text-4xl font-bold text-center mb-4'>
-              Popular Twitter Friends Locations
-            </h1>
-            <ul className='space-y-4'>
-              {locations.map((location, index) => (
-                <li key={index} className='flex justify-between items-center'>
-                  <span className='text-xl font-semibold'>{location[0]}</span>
-                  <span className='bg-blue-500 text-white font-semibold py-1 px-3 rounded'>
-                    {location[1]}
-                  </span>
-                </li>
-              ))}
-            </ul>
+      <>
+        <Head>
+          <title>My Custom Page Title</title>
+        </Head>
+        <div className='min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12'>
+          <div className='relative py-3 sm:max-w-xl sm:mx-auto'>
+            <div className='absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl'></div>
+            <div className='relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20'>
+              <h1 className='text-4xl font-bold text-center mb-4'>
+                Popular Twitter Friends Locations
+              </h1>
+              <ul className='space-y-4'>
+                {locations.map((location, index) => (
+                  <li key={index} className='flex justify-between items-center'>
+                    <span className='text-xl font-semibold'>{location[0]}</span>
+                    <span className='bg-blue-500 text-white font-semibold py-1 px-3 rounded'>
+                      {location[1]}
+                    </span>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
   return null;
