50f2dfee2afa09dc3072b76c502aed9ca95b865c
Mohamed Oun <mohamed3on@gmail.com>
2023-04-13 15:20:48 +0200

    try again

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,11 @@ import { Inter } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
 
 import type { AppProps } from 'next/app';
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={inter.className}>
+    <main className={`${inter.variable} font-sans`}>
       <Component {...pageProps} />
       <Analytics />
     </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
@@ -12,7 +15,7 @@ module.exports = {
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       fontFamily: {
-        sans: ['Inter', 'sans-serif'],
+        sans: ['var(--font-inter)', ...fontFamily.sans],
       },
     },
   },
