b35b40591094b9fe3ecf90a8f808a7d4c3e7c365
Mohamed Oun <mohamed3on@gmail.com>
2023-04-13 15:14:07 +0200

    maybe font works now?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,16 @@
 import '@/styles/globals.css';
-import '@fontsource/inter/400.css';
-import '@fontsource/inter/700.css';
+import { Inter } from 'next/font/google';
+
 import { Analytics } from '@vercel/analytics/react';
 
 import type { AppProps } from 'next/app';
+const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <main className={inter.className}>
       <Component {...pageProps} />
       <Analytics />
-    </>
+    </main>
   );
 }
