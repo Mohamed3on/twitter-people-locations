876e9a710170bf5cecafc9cc474689528f0e0fa7
Mohamed Oun <mohamed3on@gmail.com>
2023-06-19 20:05:21 +0200

    no more api access, F elon

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,11 @@ import { LocationList } from '@/components/LocationList';
 import Head from 'next/head';
 import React from 'react';
 import ThemeToggle from '@/components/ThemeToggle';
-type Locations = [string, number][];
+import locationData from './locations.json';
 
-interface HomeProps {
-  locations: Locations | null;
-  error: string | null;
-}
+const locations = locationData as [string, number][];
 
-export default function Home({ locations, error }: HomeProps) {
-  if (error) {
-    return <div className='text-center mt-20'>Error: {error}</div>;
-  }
-
+export default function Home() {
   return (
     <>
       <Head>
@@ -24,36 +17,8 @@ export default function Home({ locations, error }: HomeProps) {
         <div className='fixed top-4 right-4'>
           <ThemeToggle />
         </div>
-        {locations ? (
-          <LocationList locations={locations} />
-        ) : (
-          <div className='text-center mt-20 text-white dark:text-gray-200'>Error: {error}</div>
-        )}
+        <LocationList locations={locations} />
       </div>
     </>
   );
-}
-
-export async function getServerSideProps({ req, res }: any) {
-  try {
-    res.setHeader('Cache-Control', 's-maxage=172800, stale-while-revalidate, stale-if-error');
-    const response = await fetch(`${process.env.NEXT_PUBLIC_FUNCTIONS_BASE_URL}/api/locations`);
-    if (!response.ok) {
-      throw new Error('Failed to fetch data');
-    }
-    const locations = await response.json();
-    return {
-      props: {
-        locations,
-        error: null,
-      },
-    };
-  } catch (error: any) {
-    return {
-      props: {
-        locations: null,
-        error: error.message,
-      },
-    };
-  }
 }
